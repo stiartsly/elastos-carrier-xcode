6e69c68c5b6d6a921c3a363322d74bab06c0635a
stiartsly <stiartsly@gmail.com>
2017-12-30 16:20:45 +0800

    added apps

---
+++ apps/pfd/config.h
@@ -0,0 +1,51 @@
+#ifndef __PFD_CONFIG_H__
+#define __PFD_CONFIG_H__
+
+#include <ela_session.h>
+#include <linkedhashtable.h>
+
+#define MODE_CLIENT     1
+#define MODE_SERVER     2
+
+typedef struct {
+    HashEntry he;
+    char *name;
+    char *host;
+    char *port;
+} PFService;
+
+typedef struct {
+    HashEntry he;
+    char *userid;
+    char *services[0];
+} PFUser;
+
+typedef struct {
+    char *ipv4;
+    char *ipv6;
+    char *port;
+    char *address;
+} BootstrapNode;
+
+typedef struct {
+   bool udp_enabled;
+
+    int bootstraps_size;
+    BootstrapNode **bootstraps;
+
+    int loglevel;
+    char *logfile;
+
+    char *datadir;
+
+    int mode;
+    char *serverid;
+    char *server_address;
+
+    Hashtable *services;
+    Hashtable *users;
+} PFConfig;
+
+PFConfig *load_config(const char *config_file);
+
+#endif /* __PFD_CONFIG_H__ */
